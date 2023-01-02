bbedec3ec07ee6023e7b87687584f04e5f297c16
DEBASISH CHAKRABORTY <debasish2392000@gmail.com>
2023-01-02 19:41:56 +0530

    update

--- diceRoll/index.js
+++ diceRoll/index.js
@@ -1,11 +1,16 @@
-var randomNumeber1 = Math.floor(Math.random() * 6) + 1;
-var diceImage = "assets/dice" + randomNumeber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", diceImage);
+document.querySelector("button").addEventListener('click', function () {
+    var audio = document.querySelector("audio");
+    audio.play();
 
-var randomNumeber2 = Math.floor(Math.random() * 6) + 1;
-var diceImage = "assets/dice" + randomNumeber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", diceImage);
+    var randomNumeber1 = Math.floor(Math.random() * 6) + 1;
+    var diceImage = "assets/dice" + randomNumeber1 + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", diceImage);
 
-if (randomNumeber1 > randomNumeber2) document.querySelector("h1").textContent = "Team 🅰️ wins 🚩";
-else if (randomNumeber1 < randomNumeber2) document.querySelector("h1").textContent = "Team 🅱️ wins 🚩";
-else document.querySelector("h1").textContent = "!! It's DRAW 'Roll again' !!"
+    var randomNumeber2 = Math.floor(Math.random() * 6) + 1;
+    var diceImage = "assets/dice" + randomNumeber2 + ".png";
+    document.querySelectorAll("img")[1].setAttribute("src", diceImage);
+
+    if (randomNumeber1 > randomNumeber2) {document.querySelector("h1").textContent = "Team 🅰️ wins 🚩";
+    else if (randomNumeber1 < randomNumeber2) document.querySelector("h1").textContent = "Team 🅱️ wins 🚩";
+    else document.querySelector("h1").textContent = "!! It's DRAW 'Roll again' !!"
+});
